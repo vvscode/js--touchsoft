6338353ba20978b8ae46532b307a8be586e90137
Leonid <leonidius123@mail.ru>
2018-05-23 00:09:31 +0300

    Update chat.js

--- task-01/task/chat.js
+++ task-01/task/chat.js
@@ -1,28 +1,41 @@
-
-
 var createChat = function() {
+
+    createStyles();
+
     var chat = document.createElement('div');
     chat.id = 'chat';
-    // chat.style.position='fixed';
-    chat.setAttribute('position', 'fixed');
-    chat.setAttribute('bottom', '20');
+    chat.classList.add('chat');
     chat.style.bottom='15px';
     chat.style.right='15px';
 
     var chatName = createChatName();
     var logArea = createLogArea();
     var btn = createSendButton();
-    var textArea = createTextArea();
+    var textInput = createTextInput();
     var minimize = createMinimize();
 
     chat.appendChild(chatName);
     chat.appendChild(minimize);
     chat.appendChild(logArea);
-    chat.appendChild(textArea);
+    chat.appendChild(textInput);
     chat.appendChild(btn);
     document.body.appendChild(chat);
 }
 
+var createStyles = function () {
+    var style = document.createElement('style');
+    style.innerHTML = '\n.chat {\n  width: 500px;\n  height: 300px;\n  ' +
+        'border: 5px solid red;\n  position: fixed;\n  right: 0px;\n  ' +
+        'bottom: 0px;\n  background-color: yellow;\n}\n' +
+        '\n.minimized {\nheight: 50px;\n border: 5px solid grey;\n' +
+        'background-color: blue;\n}' +
+        '.logArea {\nheight: 100px;\n overflow: scroll;\n}' +
+        '.notShow {\ndisplay: none;\n}\n';
+    document.body.appendChild(style);
+
+
+
+}
 
 var addHistoryToPage = function() {
     var objectFormLocalStorage = localStorage.getItem("messages");
@@ -36,19 +49,39 @@ var addHistoryToPage = function() {
     }
     var logArea = document.getElementById('logArea');
     messages.forEach(function (element) {
-        var message = new Message(new Date(element.time), element.sender, element.text);
+            var message = new Message(new Date(element.time), element.sender, element.text);
             logArea.innerHTML += '<br>' + message.printMessage();
-    }
+        }
     );
-    // alert(messages);
+}
+
+var openChat = function () {
+    var isMaxChat = localStorage.getItem('isMaxChat');
+    if (isMaxChat === 'true') {
+        maximizeChat();
+    } else {
+        minChat();
+    }
+}
+
+var resizeChat = function () {
+    var isMaxChat = localStorage.getItem('isMaxChat');
+    if (isMaxChat === 'true') {
+        localStorage.setItem('isMaxChat', 'false');
+        minChat();
+    } else {
+        localStorage.setItem('isMaxChat', 'true');
+        maximizeChat();
+    }
 }
 
 window.addEventListener('load', createChat);
 window.addEventListener('load', addHistoryToPage);
+window.addEventListener('load', openChat);
 
 
 var createChatName = function () {
-    var chatName = document.createElement('h3');
+    var chatName = document.createElement('p');
     chatName.innerHTML = 'Chat with us';
     return chatName;
 }
@@ -62,21 +95,50 @@ var createMinimize = function () {
     minimize.value='button';
     minimize.innerHTML = '-';
     minimize.id = 'minButton';
-    minimize.addEventListener('click', min);
+    minimize.addEventListener('click', resizeChat);
     return minimize;
 }
 
-var min = function () {
+var minChat = function () {
     var chat = document.getElementById('chat');
-    chat.style.height = '15px';
+    chat.classList.add('minimized');
 
+    var minButton = document.getElementById('minButton');
+    minButton.innerHTML = '[]';
+
+    var button = document.getElementById('button');
+    button.classList.add('notShow');
+
+    var logArea = document.getElementById('logArea');
+   logArea.classList.add('notShow');
+
+    var textInput = document.getElementById('textInput');
+    textInput.classList.add('notShow');
+}
+
+var maximizeChat = function () {
+    var chat = document.getElementById('chat');
+    chat.classList.remove('minimized');
+
+
+    var minButton = document.getElementById('minButton');
+    minButton.innerHTML = '-';
+
+    var button = document.getElementById('button');
+    button.classList.remove('notShow');
+
+    var logArea = document.getElementById('logArea');
+    logArea.classList.remove('notShow');
+
+    var textInput = document.getElementById('textInput');
+    textInput.classList.remove('notShow');
 }
 
 
 var createLogArea = function () {
     var logArea = document.createElement('div');
     logArea.style.height = "150px";
-    logArea.class = 'logArea';
+    logArea.classList.add('logArea');
     logArea.id = 'logArea';
     logArea.innerHTML = 'our messages';
     return logArea;
@@ -85,6 +147,7 @@ var createLogArea = function () {
 
 var createSendButton = function () {
     var btn = document.createElement('button');
+    btn.id = 'button';
     btn.style.height="80px";
     btn.style.padding= "5px";
     btn.type='button';
@@ -92,17 +155,14 @@ var createSendButton = function () {
 
     btn.innerHTML = 'send message';
     btn.addEventListener("click", onSend);
-    // btn.setAttribute("position", "fixed");
-    // btn.setAttribute("bottom", "15");
-    // btn.setAttribute("right", "15");
     return btn;
 }
 
-var createTextArea = function () {
+var createTextInput = function () {
     var input = document.createElement("textArea");
-    input.id = 'inputArea';
+    input.id = 'textInput';
     input.type = "text";
-    input.className = "inputArea";
+    input.className = "textInput";
     input.setAttribute("rows", "5");
     input.setAttribute("cols", "30");
     input.style.bottom = '5px';
@@ -114,7 +174,7 @@ var createTextArea = function () {
     return input;
 }
 var onSend = function () {
-  var text = document.getElementById('inputArea');
+  var text = document.getElementById('textInput');
   addMessage(text.value);
   addAnswer(text.value);
   text.value = '';
