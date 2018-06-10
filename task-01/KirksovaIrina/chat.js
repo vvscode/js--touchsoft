0f48f194083512399f04accadd0ef156c4d62bf1
KirksovaIrina <kirksova.irina@gmail.com>
2018-06-10 23:44:55 +0300

    task 1, update

--- task-01/KirksovaIrina/chat.js
+++ task-01/KirksovaIrina/chat.js
@@ -1,6 +1,13 @@
-'use strict';
-
+var chatArea = null;
+var messageArea = null;
+var textArea = null;
+var inputText = null;
+var historyChat = [];
 var styles = document.createElement('style');
+var buttonSend = document.createElement('button');
+var div = document.createElement('div');
+var buttonMinimized = document.createElement('button');
+var title = document.createElement('h1');
 document.body.appendChild(styles);
 styles.innerHTML = '\n.chat {\n width: 300px;\n height: 300px;\n border: 3px solid silver;\n position: fixed;\n right: 0px;\n bottom: 0px;\n background-color: salmon;\n}\n' +
     '.titleMax {\n position: fixed;\n right: 260px;\n bottom: 280px;\n font-size: 1.3em;\n color: dimgray;\n}\n' +
@@ -13,26 +20,18 @@ styles.innerHTML = '\n.chat {\n width: 300px;\n height: 300px;\n border: 3px sol
     '.inputText {\n width: 240px;\n height: 30px; \n  position: fixed;\n  right: 55px;\n  bottom: 10px;\n}\n' +
     '.buttonSend {\n width: 40px;\n height: 36px; \n  position: fixed;\n  right: 10px;\n  bottom: 10px;\n}\n' +
     '.hidden {\n display: none;\n}\n';
-var chatArea = null;
-var messageArea = null;
-var textArea = null;
-var buttonSend = document.createElement('button');
-var inputText = null;
-var historyChat = [];
-var div = document.createElement('div');
 div.id = 'chat';
-var title = document.createElement('h1');
 title.id = 'title';
 title.innerHTML = 'Chat';
 div.appendChild(title);
 document.body.appendChild(div);
-var buttonMinimized = document.createElement('button');
 buttonMinimized.id = 'buttonMinimized';
 buttonMinimized.type = 'focus';
 buttonMinimized.innerHTML = '-';
 div.appendChild(buttonMinimized);
 
-var chat = function () {
+function chat () {
+    var textElement = null;
     localStorage.setItem('view', 'max');
     div.classList.remove('chatMin');
     buttonMinimized.classList.remove('buttonMinimizedMin');
@@ -65,16 +64,16 @@ var chat = function () {
         historyChat = localStorage.getItem('history').split('^&*$#%,');
         historyChat[historyChat.length - 1] = historyChat[historyChat.length - 1].split('^&*$#%')[0];
         for (var i = 0; i < historyChat.length; i++) {
-            var textElement = document.createElement('p');
+            textElement = document.createElement('p');
             textElement.innerHTML = historyChat[i];
             messageArea.appendChild(textElement);
             messageArea.scrollTop = messageArea.scrollHeight;
             historyChat[i] = historyChat[i].concat('^&*$#%');
         }
     }
-};
+}
 
-var chatMin = function () {
+function chatMin () {
     localStorage.setItem('view', 'min');
     if (chatArea) {
         div.removeChild(chatArea);
@@ -85,7 +84,7 @@ var chatMin = function () {
     div.classList.add('chatMin');
     buttonMinimized.classList.add('buttonMinimizedMin');
     title.classList.add('titleMin');
-};
+}
 
 function createChat() {
     if (localStorage.getItem('view') === 'max') {
@@ -103,7 +102,7 @@ function saveHistoryChat() {
 
 function bot(messageText) {
     var now = new Date();
-    var message = now.toLocaleTimeString().concat(' Bot: Ответ на "', messageText.toUpperCase(), '\"');
+    var message = now.toLocaleTimeString().concat(' Bot: Ответ на "', messageText.toUpperCase(), '"');
     var textElement = document.createElement('p');
     textElement.innerHTML = message;
     messageArea.appendChild(textElement);
@@ -128,9 +127,9 @@ function printMessage(messageText) {
 
 buttonMinimized.addEventListener('click', function () {
     (div.classList[0] === "chat") ? chatMin() : chat()
-}, true);
+});
 
 buttonSend.addEventListener('click', function () {
     printMessage(inputText.value);
     inputText.value = '';
-}, true);
+});
