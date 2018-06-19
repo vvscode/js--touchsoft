f017f77067fabdc4cafe97b1f1e6e1ef6dce13e7
Oksana Rodionova <gorshokov@mail.ru>
2018-06-19 18:27:20 +0300

    Complete unpromisify

---
+++ task-04/Besomhead/unpromisify.js
@@ -0,0 +1,21 @@
+/**
+ * Написать фукнцию обратную promisify
+ *
+ * она принимает фукнцию, которая возвращает результат в виде promise
+ * и возвращает обертку работающую на error-first коллбэке
+ */
+
+function unpromisify(promisified) {
+  return function wrapper() {
+    var args = Array.from(arguments);
+    var callback = args.pop();
+    promisified.apply(this, args).then(
+      function resolve(promiseData) {
+        callback(null, promiseData);
+      },
+      function reject(promiseData) {
+        callback(promiseData);
+      }
+    );
+  };
+}
