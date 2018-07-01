6a4f0df7cb77ae496d80a4146d2034d876e5661a
Andrei Danilenka <32581729+north911@users.noreply.github.com>
2018-07-02 00:03:37 +0300

    Add files via upload

---
+++ north911.task-03/agent.js
@@ -0,0 +1,96 @@
+﻿function addNewUser() {
+
+    var tab = document.getElementById('usersTab');
+    var count = 0;
+    var keys = new Array();
+    fetch('https://touchsoftchat.firebaseio.com/users.json').then(function (r) {
+        return r.json();
+    }).then(function (obj) {
+        keys = Object.keys(obj);
+        return Object.values(obj);
+    }).then(function (list) {
+        return list.map(function (i) {
+            var opt = document.createElement('option');
+            opt.value = keys[count];
+            opt.text = 'user' + count;
+            count++;
+            tab.appendChild(opt);
+            return i;
+        });
+    }).then(console.log);
+
+    var select = document.getElementById('usersTab');
+    select.addEventListener("change", addChatLogFromFireBase);
+}
+
+var addChatLogFromFireBase = function () {
+
+    document.getElementById('logArea').textContent='';
+    var logArea = document.getElementById('logArea');
+    var userID = document.getElementById('usersTab').value;
+    fetch('https://touchsoftchat.firebaseio.com/users/' + userID +
+        '/messages.json').then(function (r) {
+        return r.json();
+    }).then(function (obj) {
+        return Object.values(obj);
+    }).then(function (list) {
+        return list.map(function (i) {
+            logArea.innerHTML += i[0].message.replace('<br>', '');
+            return i[0];
+        });
+    }).then(console.log);
+}
+
+var sendButtonController = function () {
+
+    var content = document.getElementById('msgInput').value;
+    sendMessage('Agent', content);
+}
+
+var sendMessage = function (sender, message) {
+
+    var chatLog = document.getElementById('logArea');
+    var date = new Date;
+
+    var hours = date.toLocaleTimeString();
+    chatLog.innerHTML += hours;
+
+    chatLog.innerHTML += " " + sender + ': ' + message + '\n';
+    document.getElementById('msgInput').value = '';
+    var userId = document.getElementById('usersTab').value;
+    saveMessageToFireBase(hours + " " + sender + ': ' + message +'\n'+ '<br>', userId);
+}
+
+var saveMessageToFireBase = function (message, userId) {
+
+    fetch('https://touchsoftchat.firebaseio.com/users/' + userId + '/messages.json', {
+        method: 'POST',
+        body: JSON.stringify([
+            {
+                message: message,
+            },
+        ]),
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+        },
+    })
+        .then(function (response) {
+            return response.json();
+        })
+        .then(console.log);
+}
+
+var chatInit = function () {
+    var sendBtn = document.getElementById('sendBtn');
+    sendBtn.addEventListener('click', sendButtonController)
+}
+
+setInterval(function(){
+    addChatLogFromFireBase();
+}, 5000);
+
+window.addEventListener('load', addNewUser);
+window.addEventListener('load', chatInit);
+
+
---
+++ north911.task-03/conf.js
@@ -0,0 +1,35 @@
+﻿function changeCode() {
+
+    var codeTranscript = document.getElementById('codeTranscript');
+    var chatTitle = document.getElementById('chatTitle').value;
+    var botName = document.getElementById('botName').value;
+    var chatURL = document.getElementById('chatURL').value;
+    var cssClass = document.getElementById('cssClass').value;
+    var position = document.getElementById('position').value;
+    var allowMin = document.getElementById('allowMin').checked;
+    var allowDrag = document.getElementById('allowDrag').checked;
+    var reqName = document.getElementById('reqName').checked;
+    var showTime = document.getElementById('showTime').checked;
+    var requestType = document.querySelector('input[name="optionsRadiosInline"]:checked').value;
+
+
+    codeTranscript.innerHTML = '';
+    codeTranscript.innerHTML += '<script>\n';
+    codeTranscript.innerHTML += 'function createConfigObject (){\n' + 'var configParams = {\n' +
+        'chatTitle : \"' + chatTitle + '\",\n' +
+        'botName : \"' + botName + '\",\n' +
+        'chatURL : \"' + chatURL + '\",\n' +
+        'cssClass : \"' + cssClass + '\",\n' +
+        'position : \"' + position + '\",\n' +
+        'allowMin : \"' + allowMin + '\",\n' +
+        'allowDrag : \"' + allowDrag + '\",\n' +
+        'reqName : \"' + reqName + '\",\n' +
+        'showTime : \"' + showTime + '\"\n' +
+        'requestType : \"' + requestType + '\",\n' +
+        '};\n' +
+        'return configParams;\n}\n';
+    codeTranscript.innerHTML += '</script>\n';
+
+}
+
+
