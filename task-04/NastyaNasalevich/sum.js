8bb3f8f99df40555d4b61c7c6b8561628338218f
NastyaNasalevich <nastya.nasalevich@gmail.com>
2018-07-07 00:20:12 +0300

    task 4

---
+++ task-04/NastyaNasalevich/sum.js
@@ -0,0 +1,35 @@
+/* exported sum */
+
+/**
+ * Написать фукнцию сумматор, которая будет работать
+ * var s = sum();
+ * console.log(s); // 0
+ * console.log(s(1)); // 1
+ * console.log(s(1)(2)); //3
+ * console.log(s(3)(4)(5)); // 12
+ * Число вызовов может быть неограниченым
+ */
+
+ function sum(result) {
+   var total = result;
+
+    if (!total) {
+        total = 0;
+    } 
+
+    function totalSum(a) {
+        var arg = a;
+
+        if (!arg) {
+            arg = 0;
+        } 
+
+        return sum(total + arg);
+    };
+
+    totalSum.valueOf = function setTotal() {
+        return total;
+    };
+
+    return totalSum;
+}
