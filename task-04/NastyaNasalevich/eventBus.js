0988c7faa05ecdac5aea1b18590517541465554f
NastyaNasalevich <36502538+NastyaNasalevich@users.noreply.github.com>
2018-07-08 08:19:55 +0300

    Task4 (#69)
    * first commit
    
    * added a link on the working code
    
    * added tests, fixed bugs
    
    * test
    
    * fixed bags at my.tests.js
    
    * test
    
    * test
    
    * task 2
    
    * task2
    
    * task 2
    
    * task 2
    
    * task 2
    
    * task2
    
    * task 2
    
    * task 2
    
    * task 2
    
    * task 2
    
    * task 2
    
    * cc
    
    * task 3
    
    * task 3
    
    * task 3
    
    * task 3
    
    * task 3
    
    * task 3
    
    * task 3
    
    * fix bags
    
    * fix bags
    
    * fix bags
    
    * task 4
    
    * update package.json at task-4

---
+++ task-04/NastyaNasalevich/eventBus.js
@@ -0,0 +1,35 @@
+/**
+ * Написать реализацию eventBus в соответствии с тестами
+ */
+function EventBus () {}
+
+EventBus.prototype.trigger = function trigger(event, arg) {
+    var args = [].slice.call(arguments, 1);
+    this[event] = this[event] || [];
+    this[event].forEach(function f(cb) {
+            cb.apply(this, args);
+    });
+    return arg;
+};
+
+EventBus.prototype.on = function on(event, callback) {
+    
+    if(typeof callback !== "function") {
+        return;
+    }
+
+    this[event] = this[event] || [];
+    this[event].push(callback);
+};
+
+EventBus.prototype.off = function off(event, cb) {
+    
+    if (this[event]) {
+        this[event] = this[event].filter(function f(el) {
+            return el !== cb;
+        });
+    }
+
+};
+
+
