e2718bddb7075ad9eba7492e7e2afa906ca8f392
Oksana Rodionova <gorshokov@mail.ru>
2018-07-22 17:38:59 +0300

    Add tests

---
+++ task-06/Besomhead/test/get_service_markup.js
@@ -0,0 +1,15 @@
+window.addEventListener("load", function getChatConfiguratorMarkUp() {
+  var xhr = new XMLHttpRequest();
+  xhr.open(
+    "GET",
+    "https://rawgit.com/Besomhead/js--touchsoft/besomhead-task06-build/task-06/Besomhead/build/html/service.html",
+    false
+  );
+  xhr.addEventListener("load", function onLoad() {
+    document.getElementById("service-container").innerHTML = xhr.responseText;
+  });
+  xhr.addEventListener("error", function onError() {
+    throw new Error(xhr.statusText);
+  });
+  xhr.send();
+});
