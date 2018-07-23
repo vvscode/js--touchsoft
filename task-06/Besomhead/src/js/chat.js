da9c1d43cd07ee543b80a96b16a10a212baf1f20
Oksana Rodionova <gorshokov@mail.ru>
2018-07-23 19:58:45 +0300

    Besomhead. Task06 (#83)
    * Add long polling to chat and service
    
    * Fix link in README
    
    * Add tests
    
    * Fix ids in config factory

---
+++ task-06/Besomhead/src/js/chat.js
@@ -0,0 +1,35 @@
+/* exported config */
+/* global chatFactory */
+
+var MARKUP_TIMEOUT = 5000;
+
+var config = {
+  chatTitle: "Чат",
+  chatState: "",
+  userName: "",
+  chatURL: "https://besomhead-chat.firebaseio.com/",
+  cssClass: "chat-container",
+  position: "right",
+  allowMinimize: "true",
+  allowDrag: "false",
+  requireName: "false",
+  showDateTime: "true",
+  requests: "fetch",
+  updates: "refetch",
+  messagesLength: "0"
+};
+
+//= util/DOM_manager.js
+//= util/message_factory.js
+//= util/storage_manager.js
+//= util/updates_manager.js
+//= factories/chat_factory.js
+
+window.addEventListener("load", function initPage() {
+  chatFactory.appendStylesheet();
+  chatFactory.setUniqueUserID();
+  chatFactory.createChatMarkup();
+  setTimeout(chatFactory.subscribeOnUpdates, MARKUP_TIMEOUT);
+});
+
+chatFactory.setConfig();
